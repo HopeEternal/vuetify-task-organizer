59be7a8545477db0c1a071b09e094c8b6102c94c
HopeEternal <risalee316@gmail.com>
2020-01-07 13:09:44 -0500

    Navbar with signout button drafted

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <v-app-bar flat>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Task</span>
+        <span>Organizer</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey" right>
+        <span>Sign Out</span>
+        <v-icon right>mdi-logout-variant</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
