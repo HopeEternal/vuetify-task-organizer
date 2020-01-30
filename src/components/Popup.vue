a7b6fe607838f62fb0a4ce35562a08d0342eed34
HopeEternal <risalee316@gmail.com>
2020-01-30 18:18:14 -0500

    Date picker calendar added to 'Add Project' form

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -5,11 +5,62 @@
         <v-btn class="success" dark v-on="on">Add New Project</v-btn>
       </template>
       <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>Add a New Project</v-card-title>
+        <v-card-title class="headline grey lighten-2" primary-title
+          >Add a New Project</v-card-title
+        >
+        <v-card-text>
+          <v-form class="px-3">
+            <v-text-field
+              label="Title"
+              v-model="title"
+              prepend-icon="mdi-folder"
+            ></v-text-field>
+            <v-textarea
+              label="Information"
+              v-model="content"
+              prepend-icon="mdi-pencil"
+            ></v-textarea>
+
+            <v-menu max-width="290">
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  :value="formattedDate"
+                  label="Due date"
+                  prepend-icon="mdi-calendar-range"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker v-model="due"></v-date-picker>
+            </v-menu>
+
+            <v-btn text class="success mx-0 mt-3" @click="submit"
+              >Add Project</v-btn
+            >
+          </v-form>
+        </v-card-text>
       </v-card>
     </v-dialog>
   </div>
 </template>
 <script>
-export default {};
+import format from 'date-fns/format';
+export default {
+  data() {
+    return {
+      title: '',
+      content: '',
+      due: null
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.title, this.content);
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? format(parseInt(this.due), 'do MMM yyyy') : '';
+    }
+  }
+};
 </script>
